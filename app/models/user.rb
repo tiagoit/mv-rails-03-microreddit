e049bbb72a94d6015082b32cdbee3914467c0401
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-10-25 15:21:35 -0700

    user validations added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
 class User < ApplicationRecord
+  # constants
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  # validations
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255 },
+    format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 8 }
+
+  # relations
   has_secure_password
+
+  # callbacks
+  before_save do
+    email.downcase!
+  end
 end
